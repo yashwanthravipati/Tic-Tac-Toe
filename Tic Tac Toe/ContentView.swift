cac67e3e9c65ee36cc2491fa244534328293b0f3
Yashwanth Ravipati <yashwanth.ravipati@icloud.com>
2021-07-27 12:20:34 -0500

    Completed checkForWinner function

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct ContentView: View {
     @State private var moves = Array(repeating: "", count: 9)
     @State private var xTurn = true
+    @State private var gameOver = false
+    @State private var winMessage = ""
     var body: some View {
         VStack {
             Text("Tic Tac Toe")
@@ -18,55 +20,56 @@ struct ContentView: View {
                 .padding()
             LazyVGrid(columns: Array(repeating: GridItem(.fixed(120), spacing: 15), count: 3), spacing: 15, content: {
                 ForEach(0..<9) { index in
-                    
                     ZStack {
-                        
                         Color.blue
                         Color.white
                             .opacity(moves[index] == "" ? 1 : 0)
-                        
                         Text(moves[index])
-                            
                             .font(.system(size: 90))
-                            
                             .fontWeight(.heavy)
-                        
                     }
-                    
                     .frame(width: 120, height: 120, alignment: .center)
-                    
                     .cornerRadius(30)
                     .onTapGesture {
-                        
                         withAnimation(Animation.default) {
-                            
                             if moves[index] == "" {
-                                
                                 moves[index] = xTurn ? "X" : "O"
-                                
                                 xTurn.toggle()
-                                
                             }
-                            
                         }
-                        
                     }
                     .rotation3DEffect(
                         .init(degrees: moves[index] != "" ? 180 : 0),
                         axis: (x: 0.0, y: 1.0, z: 0.0)
                     )
-                    
-                    
                 }
-                
             })
             .preferredColorScheme(.dark)
-        }
-    }
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView()
+            .alert(isPresented: $gameOver, content: {
+                Alert(title: Text(winMessage))
+            })
+            .onChange(of: moves, perform: { value in
+                checkForWinner()
+            })
         }
     }
     
+    private func checkForWinner() {
+        checkLine(a: 0, b: 1, c: 2) // top row
+        checkLine(a: 3, b: 4, c: 5) // middle row
+        checkLine(a: 6, b: 7, c: 8) // bottom row
+    }
+    private func checkLine(a: Int, b: Int, c: Int) {
+        if moves[a] != "" && moves[a] == moves[b] && moves[b] == moves[c] {
+            winMessage = "\(moves[a]) is the winner!"
+            gameOver = true
+        }
+    }
 }
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
