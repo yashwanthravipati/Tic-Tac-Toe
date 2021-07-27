336f21fd86340abd3fd3d8372b020883b26b84ad
Yashwanth Ravipati <yashwanth.ravipati@icloud.com>
2021-07-27 12:24:48 -0500

    Corrected Completed checkForWinner function

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -46,7 +46,8 @@ struct ContentView: View {
             })
             .preferredColorScheme(.dark)
             .alert(isPresented: $gameOver, content: {
-                Alert(title: Text(winMessage))
+                Alert(title: Text(winMessage), dismissButton: .destructive(Text("Play again"), action: {
+                }))
             })
             .onChange(of: moves, perform: { value in
                 checkForWinner()
@@ -58,6 +59,11 @@ struct ContentView: View {
         checkLine(a: 0, b: 1, c: 2) // top row
         checkLine(a: 3, b: 4, c: 5) // middle row
         checkLine(a: 6, b: 7, c: 8) // bottom row
+        checkLine(a: 0, b: 3, c: 6)
+        checkLine(a: 1, b: 4, c: 7)
+        checkLine(a: 2, b: 5, c: 8)
+        checkLine(a: 0, b: 4, c: 8)
+        checkLine(a: 2, b: 4, c: 6)
     }
     private func checkLine(a: Int, b: Int, c: Int) {
         if moves[a] != "" && moves[a] == moves[b] && moves[b] == moves[c] {
