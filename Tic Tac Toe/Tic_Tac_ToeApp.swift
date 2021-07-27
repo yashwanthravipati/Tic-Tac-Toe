9d480a735cedb8e0da3bd1b5f22b87ce432accbd
Yashwanth Ravipati <yashwanth.ravipati@icloud.com>
2021-07-27 11:16:17 -0500

    Initial Commit

---
+++ Tic Tac Toe/Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToeApp.swift
+//  Tic Tac Toe
+//
+//  Created by Yashwanth Ravipati on 7/27/21.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
