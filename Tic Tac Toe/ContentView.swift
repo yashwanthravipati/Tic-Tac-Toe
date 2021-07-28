0efb5ba9bfd072d798514d4ee90a5ae6cfc637b4
Yashwanth Ravipati <yashwanth.ravipati@icloud.com>
2021-07-27 22:02:03 -0500

    Final Completed Finished Game

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -73,6 +73,7 @@ struct ContentView: View {
             gameOver = true
         }
     }
+    
     private func checkLine(a: Int, b: Int, c: Int) {
         if moves[a] != "" && moves[a] == moves[b] && moves[b] == moves[c] {
             winMessage = "\(moves[a]) is the winner!"
